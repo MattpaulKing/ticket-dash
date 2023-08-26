c8ea5664c91701cc98e48670a0e8bc2adebfd551
Matt King <matthewpaulking@outlook.com>
2023-08-25 19:24:25 -0700

    adding extra data so it's not all streamed

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -16,6 +16,7 @@ export const load = async ({ locals }: { locals: App.Locals }) => {
     totalEventTypeAggs.sort((a: TTotalAggType, b: TTotalAggType) => b.rank - a.rank)
 
     const monthlyEventTypeAggs = async () => {
+      random_bytes(1024)
       return await locals.supabase.rpc("get_event_type_by_calendar_month", {
         event_type_a: totalEventTypeAggs[0].eventType,
         event_type_b: totalEventTypeAggs[1].eventType,
@@ -28,7 +29,10 @@ export const load = async ({ locals }: { locals: App.Locals }) => {
         return await locals.supabase.rpc("get_event_type_table", { lim: 20, skip: 0 })
     }
 
+    const { data, error } = await locals.supabase.from("sgEvents").select("eventId").gte("created_at", "2023-08-23") 
+
     return {
+        data,
         totalEventTypeAggs,
         streamed: {
             events: events(),
--- src/routes/upcoming/+page.server.ts
+++ src/routes/upcoming/+page.server.ts
@@ -2,9 +2,11 @@ export const load = async ({ locals }: {locals: App.Locals}) => {
     const getAnnouncedLastThreeDays = async () => {
         return await locals.supabase.rpc("get_announced_last_three_days")
     }
+    const { data, error } = await locals.supabase.from("sgEvents").select("eventId").gte("created_at", "2023-08-23") 
+
     return {
-      x: 'hi',
-        streamed: {
+      data,
+       streamed: {
             announcedLastThreeDays: getAnnouncedLastThreeDays(),
         }
     }
